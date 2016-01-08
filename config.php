173807a1a1afeccfb0715e3756a4c71a118d53c9
n_dra18 <n_dra18@yahoo.com>
2016-01-08 07:16:06 +0700

    edit config.php

--- config.php
+++ config.php
@@ -1,28 +1,28 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/furniture.com/');
+define('HTTP_SERVER', 'http://candrasudirman.16mb.com/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/furniture.com/');
+define('HTTPS_SERVER', 'http://candrasudirman.16mb.com/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/html/furniture.com/catalog/');
-define('DIR_SYSTEM', '/var/www/html/furniture.com/system/');
-define('DIR_LANGUAGE', '/var/www/html/furniture.com/catalog/language/');
-define('DIR_TEMPLATE', '/var/www/html/furniture.com/catalog/view/theme/');
-define('DIR_CONFIG', '/var/www/html/furniture.com/system/config/');
-define('DIR_IMAGE', '/var/www/html/furniture.com/image/');
-define('DIR_CACHE', '/var/www/html/furniture.com/system/cache/');
-define('DIR_DOWNLOAD', '/var/www/html/furniture.com/system/download/');
-define('DIR_UPLOAD', '/var/www/html/furniture.com/system/upload/');
-define('DIR_MODIFICATION', '/var/www/html/furniture.com/system/modification/');
-define('DIR_LOGS', '/var/www/html/furniture.com/system/logs/');
+define('DIR_APPLICATION', '/home/u152187081/public_html/catalog/');
+define('DIR_SYSTEM', '/home/u152187081/public_html/system/');
+define('DIR_LANGUAGE', '/home/u152187081/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/u152187081/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/u152187081/public_html/system/config/');
+define('DIR_IMAGE', '/home/u152187081/public_html/image/');
+define('DIR_CACHE', '/home/u152187081/public_html/system/cache/');
+define('DIR_DOWNLOAD', '/home/u152187081/public_html/system/download/');
+define('DIR_UPLOAD', '/home/u152187081/public_html/system/upload/');
+define('DIR_MODIFICATION', '/home/u152187081/public_html/system/modification/');
+define('DIR_LOGS', '/home/u152187081/public_html/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '181210');
-define('DB_DATABASE', 'opencart_furniture.com');
+define('DB_HOSTNAME', 'mysql.idhostinger.com');
+define('DB_USERNAME', 'u152187081_root');
+define('DB_PASSWORD', 'ramblekids@1234');
+define('DB_DATABASE', 'u152187081_word');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
